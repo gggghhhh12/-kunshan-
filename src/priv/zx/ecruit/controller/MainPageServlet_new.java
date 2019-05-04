dfbf2eec68d225afccf24a9fd3ed92a48bd999b1
hitchenwenhao <1827897908@qq.com>
2019-05-04 12:28:51 +0800

    11

--- src/priv/zx/ecruit/controller/MainPageServlet_new.java
+++ src/priv/zx/ecruit/controller/MainPageServlet_new.java
@@ -8,6 +8,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import priv.zx.ecruit.dao.EPPostJobDao;
 import priv.zx.ecruit.dao.NewsDao;
 import priv.zx.ecruit.model.EPPostJob;
